1f61a509d548aa39ca1e36e112d0f5c9e91cc953
Wiby Fabian Rianto <73323681+WibyFabian08@users.noreply.github.com>
2020-11-04 04:23:31 -0800

    1. Migrasi Ke React js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,54 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-button main-black-color">Add</button>
+          <h1 className="header-title">Todo List</h1>
+          <button className="header-button main-red-color"> Clear</button>
+        </section>
+
+        <section className="todo-form">
+          <form className="input-form">
+            <input type="text" className="input-todo"></input>
+            <button className="add-todo main-red-color">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning Todo</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
